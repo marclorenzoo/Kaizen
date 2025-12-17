95efa2b87ad87648f3bbc733dadb0d75b83094f3
marclorenzoo <lorenzooltramarc@gmail.com>
2025-12-17 19:16:46 +0100

    User info on sidebar

--- src/app/layout/sidebar/sidebar.ts
+++ src/app/layout/sidebar/sidebar.ts
@@ -1,11 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../core/auth/auth.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
 export class Sidebar {
+  authService = inject(AuthService);
+  currentUser$ = this.authService.currentUser$;
 
 }
